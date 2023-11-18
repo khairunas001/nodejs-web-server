697999efe919a5841bad96c88cd01d52848c9f1d
Khairunas R W <128572429+khairunas001@users.noreply.github.com>
2023-11-18 13:59:20 +0700

    Latihan Mendapatkan Body Request

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
-    
+ 
     const { method } = request;
  
     if(method === 'GET') {
@@ -11,15 +11,17 @@ const requestListener = (request, response) => {
     }
  
     if(method === 'POST') {
-        response.end('<h1>Hai!</h1>');
-    }
- 
-    if(method === 'PUT') {
-        response.end('<h1>Bonjour!</h1>');
-    }
- 
-    if(method === 'DELETE') {
-        response.end('<h1>Salam!</h1>');
+        let body = [];
+          
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
+       
+        request.on('end', () => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body);
+            response.end(`<h1>Hai, ${name}!</h1>`);
+        });
     }
 };
  
